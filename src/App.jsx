8e43d4b67dbf4745f166c8e0b4e6d0db50d08b20
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-05 05:45:52 +0800

    feat: Add game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,55 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Card from './components/Card';
 import generateCards from './helpers/cards';
 
 const App = () => {
-  const [score, setScore] = useState(0);
-  const [bestScore, setBestScore] = useState(0);
+  const [cards, setCards] = useState(generateCards());
   const [currentCard, setCurrentCard] = useState('');
+  const [chosenCards, setChosenCards] = useState([]);
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(
+    Number(localStorage.getItem('bestScore')) || 0
+  );
 
   const handleClick = (e) => {
     e.preventDefault();
-    setCurrentCard(e.target.id);
 
-    if (e.target.id !== currentCard) {
-      setScore(score + 1);
-    } else {
-      setScore(0);
-    }
+    setCurrentCard(e.target.id);
+    setCards(generateCards());
   };
 
-  console.log(currentCard);
+  useEffect(() => {
+    if (currentCard && !chosenCards.includes(currentCard)) {
+      setScore(score + 1);
+      setChosenCards([...chosenCards, currentCard]);
+    } else {
+      setChosenCards([]);
+      setScore(0);
+      setCurrentCard('');
+    }
+
+    return () => {};
+  }, [cards]);
+
+  useEffect(() => {
+    if (bestScore <= score) {
+      setBestScore(score);
+    }
+
+    return () => {};
+  }, [score]);
+
+  useEffect(() => {
+    localStorage.setItem('bestScore', bestScore);
+  }, [bestScore]);
 
   return (
     <>
       <Header score={score} bestScore={bestScore} />
       <main className='container mx-auto w-11/12 py-6 | md:pt-12'>
         <div className='grid grid-cols-2 gap-2 max-w-6xl mx-auto | md:grid-cols-5 md:gap-4'>
-          {generateCards().map((card) => {
+          {cards.map((card) => {
             return <Card key={card} image={card} handleClick={handleClick} />;
           })}
         </div>
