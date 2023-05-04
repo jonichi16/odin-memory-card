1497edc8f491fb95501e6486b8d1c8a6efe4b245
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-05 03:53:03 +0800

    style: Add media query to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,10 @@ const Header = ({ score, bestScore }) => {
   return (
     <header className='bg-red-600'>
       <div className='container mx-auto w-11/12 py-2 flex items-center'>
-        <h1 className='text-slate-50 text-xl font-bold flex-1'>Memory Game</h1>
-        <div className='text-slate-50 text-xs'>
+        <h1 className='text-slate-50 text-xl font-bold flex-1 | md:text-2xl'>
+          Memory Game
+        </h1>
+        <div className='text-slate-50 text-xs | md:text-base'>
           <p>Score: {score}</p>
           <p>Best Score: {bestScore}</p>
         </div>
